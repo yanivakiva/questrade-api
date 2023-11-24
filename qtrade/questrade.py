f312f36803eeade51c5acc2131ac1cfb3c5cfc5c
yanivakiva <yanivakiva20@gmail.com>
2023-11-24 22:07:18 +0200

    remove test

--- qtrade/questrade.py
+++ qtrade/questrade.py
@@ -1,4 +1,4 @@
-"""Core module for Questrade API wrapper. - Fork by Yaniv the king - check check"""
+"""Core module for Questrade API wrapper. - Fork by Yaniv the king"""
 
 import logging
 from typing import Any, Dict, List, Optional, Union
